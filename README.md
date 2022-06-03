b42b80093b2f3039da8a157e02bcdc13ea07ab36
Moarram <papwansam@gmail.com>
2022-06-02 20:50:34 -0700

    update README and reorganize config

--- README.md
+++ README.md
@@ -2,11 +2,11 @@
 ...
 
 ## Installation
-Requires [Node.js](https://nodejs.org/en/download/) (v14+) and npm (v6+)
+Requires [Node.js](https://nodejs.org/en/download/) (14+) and npm (6+)
 
 Clone the repository
 ```
-git clone https://github.com/Moarram/ping-and-ding.git
+git clone https://github.com/moarram/ping-and-ding.git
 ```
 
 Install dependencies
@@ -15,8 +15,85 @@ cd ping-and-ding
 npm install
 ```
 
+<br>
+
+
 ## Setup
+
+### Slack
 ...
 
-## Usage
+### Cron
 ...
+
+<br>
+
+
+## Usage
+By default the script will look for a `config.json` in the same directory as itself and will log results to `output/`, but this can be overridden with the optional command line arguments.
+
+### Once
+Run the script and exit.
+```
+node ping-and-ding.js [config_file] [output_dir]
+```
+
+### Repeat
+To run the script repeatedly you should use `cron` or some equivalent that calls the script on a schedule to ensure that it will keep working even after the machine restarts.
+
+<br>
+
+
+## Config
+The config file has the following top level structure:
+* `targets` - array of targets **(required)**
+* `notifier` - notification method **(required)**
+* `default` - settings shared across targets
+
+### Target *(Ping!)*
+A target describes the resource to request and the expected response signature. It needs a `name` and `url` at minimum, all other fields have built in defaults and are optional.
+
+* `name` - unique, human readable name for the target **(required)**
+* `url` - resource to fetch **(required)**
+* `init` - initalization object passed directly to `fetch()`
+  * `method` - request method such as `GET`, `POST`, etc
+  * `headers` - request headers, an object of header key value pairs
+  * `body` - request content, objects are automatically stringified
+* `expect` - criteria to check in the response
+  * `status` - expected response code (fail as STATUS)
+  * `headers` - expected headers, an object of header key value pairs, use `""` as the value to only check that the header exists (fail as HEADERS)
+  * `responseTime` - expected response time in ms (fail as RESPONSE_TIME)
+* `responseTimeRetries` - only notify RESPONSE_TIME failure after this many consecutive retries are all too slow
+* `timeout` - abort the request after waiting this many ms for a response (fail as TIMEOUT)
+* `truncateBody` - after STATUS failure the logged response body is shortened to this many chars
+* `notifierCooldownMins` - max notification rate for this target in minutes
+
+### Notifier *(Ding!)*
+The notification is sent by posting to your Slack web-hook. I may add more notification methods in the future, as well as the ability to notify multiple hooks at once.
+* `url` - Slack web-hook to send notification **(required)**
+* `timeout` - abort the notification attempt after waiting this many ms for response
+
+### Default
+Default values are used for fields that individual objects don't specify.
+* `target` - default target object
+
+### Examples
+The following example config will send a `GET` request to `example.com` and expects a response code of `200 OK` within `300ms`. If these conditions are not met, or no response is received in `1000ms`, a notification will be sent to the provided Slack web-hook url.
+```
+{
+  "targets": [
+    {
+      "name": "Example",
+      "url": "http://example.com",
+      "expect": {
+        "status": 200,
+        "responseTime": 300
+      },
+      "timeout": 1000,
+    }
+  ],
+  "notifier": {
+    "url": "https://hooks.slack.com/..."
+  },
+}
+```
--- ping-and-ding.js
+++ ping-and-ding.js
@@ -3,24 +3,31 @@
 /* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * 
 config.json
 {
-  "notificationUrl": <str>, // slack web-hook
-  "notificationTimeout": <num>, // ms
   "targets": [
     {
       "name": <str>, // required, unique
       "url": <str>, // required, unique
       "init": {}, // fetch params
-      "expectedStatus": <num>,
-      "expectedHeaders": {
-        <str>: <str> // use "" for any value
+      "expect": {
+        "status": <num>, // status code
+        "headers": {
+          <str>: <str> // use "" to accept any value
+        },
+        "responseTime": <num> // ms
       },
-      "maxResponseTime": <num>, // ms
+      "responseTimeRetries": <num>, // consecutive RESPONSE_TIME failures before notifying
       "timeout": <num>, // ms
-      "retries": <num>, // consecutive RESPONSE_TIME failures before reporting
-      "truncateBody": <num>, // chars of body in result
-      "notificationCooldownMinutes": <num> // minutes before sending another notification
+      "truncateBody": <num> // chars of body in result
+      "notifierCooldownMins": <num>, // minutes before sending another notification
     }
-  ]
+  ],
+  "notifier": {
+    "url": <str>, // slack web-hook
+    "timeout": <num> // ms
+  },
+  "defaults": {
+    "target": {} // defaults for optional values applied to all targets
+  }
 }
 * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * */
 
@@ -33,10 +40,10 @@ config.json
 // =============================================================================
 
 import fetch from 'node-fetch'
-import * as fs from 'fs/promises'
-import path from 'path'
-import { exit } from 'process'
-import { fileURLToPath } from 'url'
+import * as fs from 'node:fs/promises'
+import path from 'node:path'
+import process from 'node:process'
+import { fileURLToPath } from 'node:url'
 
 // =============================================================================
 
@@ -47,11 +54,14 @@ const YYYY = iso.slice(0, 4)
 const MM = iso.slice(5, 7)
 const DD = iso.slice(8, 10)
 
-const LOG_FILEPATH = path.join(__dirname, 'output', 'logs', `${YYYY}-${MM}-${DD}-ping-and-ding.log`)
-const WARN_FILEPATH = path.join(__dirname, 'output', 'warns', `${YYYY}-${MM}-${DD}-ping-and-ding-warn.log`)
-const DATA_FILEPATH = path.join(__dirname, 'output', 'data', `${YYYY}-${MM}-[name].csv`) // name populated later
-const STATE_FILEPATH = path.join(__dirname, 'state.json')
-const CONFIG_FILEPATH = path.join(__dirname, 'config.json')
+const configFilepath = process.argv[2] || 'config.json'
+const outputDir = process.argv[3] || 'output'
+
+const LOG_FILEPATH = path.resolve(__dirname, outputDir, 'logs', `${YYYY}-${MM}-${DD}-ping-and-ding.log`)
+const WARN_FILEPATH = path.resolve(__dirname, outputDir, 'warns', `${YYYY}-${MM}-${DD}-ping-and-ding-warn.log`)
+const DATA_FILEPATH = path.resolve(__dirname, outputDir, 'data', `${YYYY}-${MM}-[name].csv`) // name populated later
+const STATE_FILEPATH = path.resolve(__dirname, 'state.json')
+const CONFIG_FILEPATH = path.resolve(__dirname, configFilepath)
 
 // =============================================================================
 
@@ -72,7 +82,90 @@ const LOGGER = {
 
 // -----------------------------------------------------------------------------
 
-async function poke({ name, url, init={}, expectedStatus=200, expectedHeaders={}, maxResponseTime=500, timeout=5000, truncateBody=null }) {
+// add contents of src to dest, optionally overwriting existing values
+function mergeObj(dest, src, { overwrite=false }={}) {
+  Object.entries(src).forEach(([key, val]) => {
+    if (val && typeof val === 'object' && key in dest) {
+      mergeObj(dest[key], val, { overwrite })
+    } else if (!(key in dest) || overwrite) {
+      dest[key] = val
+    }
+  })
+}
+
+// -----------------------------------------------------------------------------
+
+async function ensureDir(dirpath) {
+  try {
+    await fs.mkdir(dirpath, { recursive: true })
+  } catch (err) {
+    LOGGER.error(err.stack)
+  }
+}
+
+async function exists(filepath) {
+  try {
+    await fs.access(filepath)
+    return true
+  } catch (err) {
+    return false
+  }
+}
+
+async function read(filepath) {
+  try {
+    return await fs.readFile(filepath, { encoding: 'utf-8' })
+  } catch (err) {
+    LOGGER.error(`Failed to read "${path.parse(filepath).base}"`)
+    LOGGER.error(err.stack)
+    return null
+  }
+}
+
+async function write(filepath, data, flag='a') {
+  try {
+    await fs.writeFile(filepath, data, { flag, encoding: 'utf-8' })
+  } catch (err) {
+    LOGGER.error(`Failed to write "${path.parse(filepath).base}"`)
+    LOGGER.error(err.stack)
+  }
+}
+
+// -----------------------------------------------------------------------------
+
+async function readJSON(filepath) {
+  const data = await read(filepath)
+  try {
+    return JSON.parse(data)
+  } catch (err) {
+    LOGGER.error(`Failed to parse "${path.parse(filepath).base}"`)
+    LOGGER.error(err.stack)
+    return null
+  }
+}
+
+async function writeData(name, result) {
+  const filepath = DATA_FILEPATH.replace('[name]', name)
+  const line = [
+    result.timestamp,
+    result.responseTime || -1,
+    result.status || 0,
+  ]
+  if (!(await exists(filepath))) {
+    await write(filepath, '"Timestamp","Response Time","Status Code"\n')
+  }
+  await write(filepath, `${line.join(',')}\n`)
+}
+
+async function writeWarn(result) {
+  await write(WARN_FILEPATH, `${JSON.stringify(result)}\n`)
+}
+
+// -----------------------------------------------------------------------------
+
+async function poke({ name, url, init={}, expect={}, timeout=5000, truncateBody=null }) {
+  expect = { status: 200, headers: {}, responseTime: 500, ...expect } // defaults
+
   const result = { name, url, timestamp: null, status: null, responseTime: null }
   
   let failed = false
@@ -87,6 +180,10 @@ async function poke({ name, url, init={}, expectedStatus=200, expectedHeaders={}
   const abortTimeout = setTimeout(() => controller.abort(), timeout)
   
   try {
+    if ('body' in init && typeof init.body === 'object') {
+      init.body = JSON.stringify(init.body)
+    }
+
     LOGGER.log(`Requesting "${url}"`)
     result.timestamp = new Date().toISOString()
     const t1 = Date.now()
@@ -97,16 +194,16 @@ async function poke({ name, url, init={}, expectedStatus=200, expectedHeaders={}
     result.status = response.status
     result.responseTime = t2 - t1
 
-    if (response.status !== expectedStatus) {
+    if (response.status !== expect.status) {
       const text = await response.text()
       failed || fail({
         type: 'STATUS',
-        description: `Didn't respond with status ${expectedStatus}`,
+        description: `Didn't respond with status ${expect.status}`,
         body: (truncateBody && text.length > truncateBody) ? `${text.slice(0, truncateBody)}...` : text
       })
     }
 
-    Object.entries(expectedHeaders).forEach(([key, val]) => {
+    Object.entries(expect.headers).forEach(([key, val]) => {
       if (!(response.headers.has(key) && (val === '' || response.headers.get(key) === val))) {
         failed || fail({
           type: 'HEADER',
@@ -116,10 +213,10 @@ async function poke({ name, url, init={}, expectedStatus=200, expectedHeaders={}
       }
     })
 
-    if (result.responseTime > maxResponseTime) {
+    if (result.responseTime > expect.responseTime) {
       failed || fail({
         type: 'RESPONSE_TIME',
-        description: `Response took longer than ${maxResponseTime}ms`,
+        description: `Response took longer than ${expect.responseTime}ms`,
       })
     }
 
@@ -147,18 +244,19 @@ async function poke({ name, url, init={}, expectedStatus=200, expectedHeaders={}
 // -----------------------------------------------------------------------------
 
 async function notify(target, result) {
+  if (!CONFIG.notifier) return
+  
+  const notifier = { ...CONFIG.notifier }
+
   const prev = Date.parse(STATE.lastNotificationTime[target.url]) || 0
   const elapsed = Date.now() - prev
-  const cooldown = target.notificationCooldownMins || 0
+  const cooldown = target.notifierCooldownMins || 0
 
   if (elapsed >= cooldown * 60 * 1000) {
     LOGGER.log(`Sending notification...`)
     
     try {
-      await notifySlack(result, {
-        url: CONFIG.notificationUrl,
-        timeout: CONFIG.notificationTimeout || 10 * 1000,
-      })
+      await notifySlack(result, notifier)
       STATE.lastNotificationTime[target.url] = new Date().toISOString()
       LOGGER.log('Notification sent')
 
@@ -221,76 +319,6 @@ async function notifySlack(result, { url, timeout }) {
   }
 }
 
-// -----------------------------------------------------------------------------
-
-async function readJSON(filepath) {
-  const data = await read(filepath)
-  try {
-    return JSON.parse(data)
-  } catch (err) {
-    LOGGER.error(`Failed to parse "${path.parse(filepath).base}"`)
-    LOGGER.error(err.stack)
-    return null
-  }
-}
-
-async function writeData(name, result) {
-  const filepath = DATA_FILEPATH.replace('[name]', name)
-  const line = [
-    result.timestamp,
-    result.responseTime || -1,
-    result.status || 0,
-  ]
-  if (!(await exists(filepath))) {
-    await write(filepath, '"Timestamp","Response Time","Status Code"\n')
-  }
-  await write(filepath, `${line.join(',')}\n`)
-}
-
-async function writeWarn(result) {
-  await write(WARN_FILEPATH, `${JSON.stringify(result)}\n`)
-}
-
-// -----------------------------------------------------------------------------
-
-async function ensureDir(dirpath) {
-  try {
-    await fs.mkdir(dirpath, { recursive: true })
-  } catch (err) {
-    LOGGER.error(err.stack)
-  }
-}
-
-async function exists(filepath) {
-  try {
-    await fs.access(filepath)
-    return true
-  } catch (err) {
-    return false
-  }
-}
-
-async function read(filepath) {
-  try {
-    return await fs.readFile(filepath, { encoding: 'utf-8' })
-  } catch (err) {
-    if (err.code !== 'ENOENT') {
-      LOGGER.error(`Failed to read "${path.parse(filepath).base}"`)
-      LOGGER.error(err.stack)
-    }
-    return null
-  }
-}
-
-async function write(filepath, data, flag='a') {
-  try {
-    await fs.writeFile(filepath, data, { flag, encoding: 'utf-8' })
-  } catch (err) {
-    LOGGER.error(`Failed to write "${path.parse(filepath).base}"`)
-    LOGGER.error(err.stack)
-  }
-}
-
 // =============================================================================
 
 for (let filepath of [LOG_FILEPATH, WARN_FILEPATH, DATA_FILEPATH]) {
@@ -299,30 +327,64 @@ for (let filepath of [LOG_FILEPATH, WARN_FILEPATH, DATA_FILEPATH]) {
 
 const CONFIG = await readJSON(CONFIG_FILEPATH)
 try {
-  if (typeof CONFIG !== 'object') throw 'config missing'
-  if (!('notificationUrl' in CONFIG)) throw 'config must have "notificationUrl" string'
+  if (!CONFIG || typeof CONFIG !== 'object') throw 'config must be an object'
   if (!('targets' in CONFIG || Array.isArray(CONFIG.targets))) throw 'config must have "targets" array'
   const usedNames = new Set()
-  const usedUrls = new Set()
   CONFIG.targets.forEach(target => {
     if (typeof target !== 'object') throw 'each target must be an object'
     if (!('name' in target || typeof target.name !== 'string')) throw 'each target must have a unique "name" string'
     if (usedNames.has(target.name)) throw `target name "${target.name}" not unique`
     usedNames.add(target.name)
-    if (!('url' in target || typeof target.url !== 'string')) throw 'each target must have a unique "url" string'
-    if (usedUrls.has(target.url)) throw `target url "${target.url}" not unique`
-    usedUrls.add(target.url)
   })
+  if (!('notifier' in CONFIG)) throw 'must have "notifier" object'
+  if ('notifier' in CONFIG && !('url' in CONFIG.notifier)) throw 'notifier must have a "url" string'
 } catch (err) {
   LOGGER.error(`Invalid config: ${err}`)
   await LOGGER.flush()
-  exit(1)
+  process.exit(1)
 }
 
-const STATE = {
-  lastNotificationTime: {},
-  ...await readJSON(STATE_FILEPATH),
+const DEFAULTS = {
+  target: {
+    init: {},
+    expect: {
+      status: 200,
+      headers: {},
+      responseTime: 2000,
+    },
+    responseTimeRetries: 0,
+    timeout: 5000,
+    notifierCooldownMins: 1,
+    truncateBody: 1000,
+  },
+  notifier: {
+    timeout: 10 * 1000,
+  },
 }
+
+// Apply defaults to config
+const targetConfig = CONFIG?.defaults?.target || {}
+for (let target of CONFIG.targets) {
+  mergeObj(target, targetConfig)
+  mergeObj(target, DEFAULTS.target)
+}
+const notifierConfig = CONFIG?.defaults?.notifier || {}
+if ('notifier' in CONFIG) {
+  mergeObj(CONFIG.notifier, notifierConfig)
+  mergeObj(CONFIG.notifier, DEFAULTS.notifier)
+}
+if ('notifiers' in CONFIG) {
+  for (let notifier of CONFIG.notifiers) {
+    mergeObj(notifier, notifierConfig)
+    mergeObj(notifier, DEFAULTS.notifier)
+  }
+}
+
+let lastState = {}
+if (await exists(STATE_FILEPATH)) {
+  lastState = await readJSON(STATE_FILEPATH)
+}
+const STATE = { lastNotificationTime: {}, ...lastState }
 
 // -----------------------------------------------------------------------------
 
@@ -333,26 +395,30 @@ for (let target of CONFIG.targets) {
   if ('failure' in result) {
     await writeWarn(result)
 
-    if (result.failure.type === 'RESPONSE_TIME' && target.retries) {
-      LOGGER.log(`Retrying ${target.retries} times...`)
+    if (result.failure.type === 'RESPONSE_TIME' && target.responseTimeRetries) {
+      LOGGER.log(`Retrying ${target.responseTimeRetries} times...`)
       let responseTimeFailures = 1
 
-      for (let i = 0; i < target.retries; i++) {
+      for (let i = 0; i < target.responseTimeRetries; i++) {
         result = await poke(target)
         await writeData(target.name, result)
 
         if ('failure' in result) {
           await writeWarn(result)
 
-          if (result.failure.type === 'RESPONSE_TIME' && target.retries) {
+          if (result.failure.type === 'RESPONSE_TIME' && target.responseTimeRetries) {
             responseTimeFailures += 1
           } else {
             await notify(target, result)
+            break
           }
+
+        } else {
+          break
         }
       }
 
-      if (target.retries && target.retries < responseTimeFailures) {
+      if (target.responseTimeRetries && responseTimeFailures > target.responseTimeRetries) {
         await notify(target, result)
       }
       
